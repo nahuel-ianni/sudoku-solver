906e1269f4460e0c43af36e9b90eb7c660182fba
nahuel-ianni <8545295+nahuel-ianni@users.noreply.github.com>
2020-03-08 02:21:09 +0100

    is_safe: Added missing logic and tests

--- src/sudoku.py
+++ src/sudoku.py
@@ -1,38 +1,20 @@
+"""[summary]
 """
-"""
-class Sudoku():
+
+def find_empty_location(grid):
     """
-    """    
-    def __init__(self, grid):
-        self.original = grid
-        self.solution = grid
+    Looks for the coordinates of the next zero value on the grid,
+    starting on the upper left corner, from left to right and top to bottom.
 
+    Keyword Arguments:
+        grid {number matrix} -- The matrix to look for the coordinates on (default: {The instance's grid})
 
-    def find_empty_location(self, grid = None):
-        """
-        Looks for the coordinates of the next zero value on the grid, 
-        starting on the upper left corner, from left to right and top to bottom.
-            
-        Keyword Arguments:
-            grid {number matrix} -- The matrix to look for the coordinates on (default: {The instance's grid})
-            
-        Returns:
-            Tuple: Row, Column -- The coordinates of the next zero value on the grid 
-            if one is found, otherwise 'None'
-        """
-        for index, row in enumerate(grid or self.solution):
-            if 0 in row:
-                return (index, row.index(0))
-
-
-    def solve(self):
-        """
-        Attempts to solve the grid following Sudoku rules, where on a 9x9 grid:
-            - Only numbers from 1 to 9 are valid
-            - No duplicates on either rows nor columns
-            - No duplicates within the special 3x3 subgrids
-        """
-        pass
+    Returns:
+        tuple -- The (x, y) coordinates of the next zero value on the grid if one is found
+    """
+    for index, row in enumerate(grid):
+        if 0 in row:
+            return (row.index(0), index)
 
 
 def is_completed(grid):
@@ -46,7 +28,7 @@ def is_completed(grid):
     Returns:
         Boolean -- True if all numbers are unique on their respective rows and columns, otherwise, False
     """
-    return all(all(cell != 0 for cell in row) for row in grid)
+    return not any(0 in row for row in grid)
 
 
 def is_safe(grid, cell, digit):
@@ -54,17 +36,36 @@ def is_safe(grid, cell, digit):
     
     Arguments:
         grid {number matrix} -- The matrix to check the digit at
-        cell {Tuple: Row, Column} -- The coordinates of the digit on the grid
+        cell {tuple} -- The (x, y) coordinates of the digit on the grid
         digit {number} -- The digit to check for
 
     Returns:
         Boolean -- True if the digit is unique on its respective row and column, otherwise, False
     """
-    x, y = cell
+    y, x = cell
 
-    if digit in grid[x] + [row[y] for row in grid]:
+    if digit in [*grid[x], *[row[y] for row in grid]]:
         return False
 
-    # TODO: Falta revisar si el numero esta en su submatriz
+    row_level = x // 3
+    col_level = y // 3
+    subgrid = []
+    
+    for index, row in enumerate(grid):
+        if index in range(row_level * 3, row_level * 3 + 3):
+            subgrid.append(row[col_level * 3 : col_level * 3 + 3])
+
+    if digit in subgrid:
+        return False
 
     return True
+
+
+def solve(grid):
+    """
+    Attempts to solve the grid following Sudoku rules, where on a 9x9 grid:
+        - Only numbers from 1 to 9 are valid
+        - No duplicates on either rows nor columns
+        - No duplicates within the special 3x3 subgrids
+    """
+    raise NotImplementedError
--- src/sudoku_test.py
+++ src/sudoku_test.py
@@ -1,22 +1,20 @@
 import unittest
 
-from sudoku import Sudoku, is_completed, is_safe
+from sudoku import find_empty_location, is_completed, is_safe, solve
 
 
 class SudokuTest(unittest.TestCase):
-    def test_sudoku_stores_original_grid(self):
-        self.assertEqual(Sudoku(self.unfinished_sudoku).original, self.unfinished_sudoku)
     
     # Find empty location function tests
 
     def test_find_empty_location_no_result(self):
-        self.assertIsNone(Sudoku([[1, 2], [3, 4]]).find_empty_location())
+        self.assertIsNone(find_empty_location([[1, 2], [3, 4]]))
 
     def test_find_empty_location_several_locations(self):
-        self.assertEqual(Sudoku([[1, 0], [0, 0]]).find_empty_location(), (0, 1))
+        self.assertEqual(find_empty_location([[1, 0], [0, 0]]), (1, 0))
 
     def test_find_empty_location_unique_location(self):
-        self.assertEqual(Sudoku([[1, 2], [3, 0]]).find_empty_location(), (1, 1))
+        self.assertEqual(find_empty_location([[1, 2], [3, 0]]), (1, 1))
 
     # Is completed function tests
 
@@ -41,16 +39,43 @@ class SudokuTest(unittest.TestCase):
     # Is safe function tests
 
     def test_is_safe_not_on_column(self):
-        self.assertFalse(is_safe([[1, 3, 5, 7, 9], [2, 4, 6, 0, 0]], (1, 3), 7))
+        self.assertFalse(is_safe([[1, 3, 5, 7, 9], [2, 4, 6, 0, 0]], (3, 1), 7))
     
     def test_is_safe_not_on_row(self):
-        self.assertFalse(is_safe([[1, 3, 5, 7, 9], [2, 4, 6, 8, 0]], (1, 3), 8))
+        self.assertFalse(is_safe([[1, 3, 5, 7, 9], [2, 4, 6, 8, 0]], (3, 1), 8))
 
     def test_is_safe_on_column(self):
-        self.assertTrue(is_safe([[1, 3, 5, 7, 9], [2, 4, 6, 0, 0]], (1, 3), 8))
+        self.assertTrue(is_safe([[1, 3, 5, 7, 9], [2, 4, 6, 0, 0]], (3, 1), 8))
         
     def test_is_safe_on_row(self):
-        self.assertTrue(is_safe([[1, 3, 5, 7, 9], [2, 4, 6, 8, 0]], (1, 3), 9))
+        self.assertTrue(is_safe([[1, 3, 5, 7, 9], [2, 4, 6, 8, 0]], (3, 1), 9))  
+
+    def test_is_safe_on_block_1(self):
+        self.assertTrue(is_safe(self.unfinished_sudoku, (2, 1), 1))
+    
+    def test_is_safe_on_block_2(self):
+        self.assertTrue(is_safe(self.unfinished_sudoku, (4, 1), 3))
+    
+    def test_is_safe_on_block_3(self):
+        self.assertTrue(is_safe(self.unfinished_sudoku, (7, 1), 1))
+    
+    def test_is_safe_on_block_4(self):
+        self.assertTrue(is_safe(self.unfinished_sudoku, (1, 4), 1))
+    
+    def test_is_safe_on_block_5(self):
+        self.assertTrue(is_safe(self.unfinished_sudoku, (3, 3), 6))
+
+    def test_is_safe_on_block_6(self):
+        self.assertTrue(is_safe(self.unfinished_sudoku, (7, 4), 1))
+    
+    def test_is_safe_on_block_7(self):
+        self.assertTrue(is_safe(self.unfinished_sudoku, (1, 7), 1))
+    
+    def test_is_safe_on_block_8(self):
+        self.assertTrue(is_safe(self.unfinished_sudoku, (4, 7), 2))
+
+    def test_is_safe_on_block_9(self):
+        self.assertTrue(is_safe(self.unfinished_sudoku, (8, 6), 9))
 
     # Utility functions
 
